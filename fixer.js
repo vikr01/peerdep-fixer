c67be35f3978b22d336422afa794cbf494105533
vikr01 <vik120@icloud.com>
2019-01-26 19:29:37 -0800

    expose api, add back main

--- fixer.js
+++ fixer.js
@@ -1,46 +1,19 @@
 const fs = require('fs');
 const path = require('path');
-const chalk = require('chalk');
-const resolve = require('resolve');
+const { fix } = require('./');
+
+const format = pkg => JSON.stringify(pkg, null, 2);
 
 const pathToPackageJson = require.resolve(
   path.join(process.cwd(), 'package.json')
 );
+
 const currentPackageJson = JSON.parse(
   fs.readFileSync(pathToPackageJson),
   'utf-8'
 );
-const { dependencies = {}, peerDependencies = {} } = currentPackageJson;
 
-const format = pkg => JSON.stringify(pkg, null, 2);
+const newPackageJson = fix(currentPackageJson, pathToPackageJson);
+const newPackageJsonRaw = format(newPackageJson);
 
-Object.keys(dependencies).forEach(depName => {
-  let pathToDepPackageJson;
-  try {
-    pathToDepPackageJson = resolve.sync(path.join(depName, 'package.json'), {
-      basedir: pathToPackageJson,
-    });
-  } catch (err) {
-    console.warn(
-      chalk.yellow(`Could not find dependency ${depName}. Skipping.`)
-    );
-    return;
-  }
-
-  const { peerDependencies: peerDepsOfDep = {} } = JSON.parse(
-    fs.readFileSync(pathToDepPackageJson)
-  );
-
-  Object.entries(peerDepsOfDep).forEach(
-    ([peerDepName, peerDepVersionRange]) => {
-      if (dependencies[peerDepName]) {
-        return;
-      }
-
-      peerDependencies[peerDepName] = peerDepVersionRange;
-    }
-  );
-});
-
-const newPackageJson = format(currentPackageJson);
-fs.writeFileSync(pathToPackageJson, newPackageJson, 'utf-8');
+fs.writeFileSync(pathToPackageJson, newPackageJsonRaw, 'utf-8');
---
+++ index.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
+const resolve = require('resolve');
+
+const fix = (pkg, pathToPackage = process.cwd()) => {
+  const { dependencies = {}, peerDependencies = {} } = pkg;
+
+  // needed to preserve key order of package.json
+  const { ...newPkg } = pkg;
+
+  Object.keys(dependencies).forEach(depName => {
+    let pathToDepPackageJson;
+    try {
+      pathToDepPackageJson = resolve.sync(path.join(depName, 'package.json'), {
+        basedir: pathToPackage,
+      });
+    } catch (err) {
+      console.warn(
+        chalk.yellow(`Could not find dependency ${depName}. Skipping.`)
+      );
+      return;
+    }
+
+    const { peerDependencies: peerDepsOfDep = {} } = JSON.parse(
+      fs.readFileSync(pathToDepPackageJson)
+    );
+
+    Object.entries(peerDepsOfDep).forEach(
+      ([peerDepName, peerDepVersionRange]) => {
+        if (dependencies[peerDepName] || peerDependencies[depName]) {
+          return;
+        }
+
+        peerDependencies[peerDepName] = peerDepVersionRange;
+      }
+    );
+  });
+
+  newPkg.dependencies = dependencies;
+  newPkg.peerDependencies = peerDependencies;
+
+  return newPkg;
+};
+
+module.exports = {
+  fix,
+};
